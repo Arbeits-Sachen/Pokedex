fc37b91e4983eac86ef95a8ea9935e27851202da
Arbeits-Sachen <105115862+Arbeits-Sachen@users.noreply.github.com>
2022-11-26 12:31:54 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import Logo from "./Logo.js"
 import BestPokemon from "./BestPokemon.js"
 import CaughtPokemon from "./CaughtPokemon.js"
+import PokemonMovesSelector from './PokemonMovesSelector';
+import PokemonCity from './PokemonCity';
 
 function App()
 {
@@ -14,6 +16,8 @@ function App()
       <Logo appName = "Kerim's" logWhenClicked = {logWhenClicked}/>
       <BestPokemon abilities = {['Anticipation', 'Adaptability', 'Run-Away']}/>
       <CaughtPokemon pokemon = {["N/A_1", "N/A_2", "N/A_3"]} date = {new Date().toLocaleDateString()}/>
+      <PokemonMovesSelector/>
+      <PokemonCity/>
     </div>
   );
 }
